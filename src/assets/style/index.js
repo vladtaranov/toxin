7726e61c8eb92d5c40c75c4a38767d1b7de8d9dc
Vladislav Taranov <vladislav.taranov@live.com>
2020-05-24 22:06:10 +0300

    Assets structure changed

--- src/assets/style/index.js
+++ src/assets/style/index.js
@@ -2,20 +2,21 @@ import './reset.scss';
 import './global.scss';
 import './fonts.scss';
 
-import '../../components/text-field/text-field.scss';
 import '../../components/calendar/calendar.scss';
+import '../../components/checkbox/checkbox.scss';
+import '../../components/checkbox.rich/checkbox.rich.scss';
+import '../../components/details/details.scss';
 import '../../components/dropdown/dropdown.scss';
 import '../../components/dropdown.dates/dropdown.dates.scss';
 import '../../components/dropdown.dates-filter/dropdown.dates-filter.scss';
-import '../../components/range/range.scss';
-import '../../components/checkbox/checkbox.scss';
-import '../../components/checkbox.rich/checkbox.rich.scss';
-import '../../components/radio/radio.scss';
-import '../../components/toggle/toggle.scss';
-import '../../components/rate/rate.scss';
 import '../../components/like/like.scss';
 import '../../components/pagination/pagination.scss';
-import '../../components/details/details.scss';
+import '../../components/radio/radio.scss';
+import '../../components/range/range.scss';
+import '../../components/rate/rate.scss';
+import '../../components/review/review.scss';
+import '../../components/text-field/text-field.scss';
+import '../../components/toggle/toggle.scss';
 
 import '../../layouts/header/header.scss';
 
