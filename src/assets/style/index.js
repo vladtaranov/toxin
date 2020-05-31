0e844030feafb72e2bf8541ef1472c854531fdeb
Vladislav Taranov <vladislav.taranov@live.com>
2020-05-31 21:36:04 +0300

    Header styles divided

--- src/assets/style/index.js
+++ src/assets/style/index.js
@@ -23,6 +23,7 @@ import '../../components/toggle/toggle.scss';
 import '../../layouts/footer/footer.scss';
 import '../../layouts/footer.minimal/footer.minimal.scss';
 import '../../layouts/header/header.scss';
+import '../../layouts/header/header.mobile.scss';
 import '../../layouts/header.ui-kit/header.ui-kit.scss';
 import '../../layouts/order-room/order-room.scss';
 import '../../layouts/room-card/room-card.scss';
